eb26c01a5343e8b327e045de7e4e9912e842618e
Lillian Wright <10803934@my.uvu.edu>
2019-11-19 16:59:24 -0700

    blocks staging and animation set

---
+++ I Spy Interactive/Assets/Scripts/AssetAction/AnimatorSetTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSetTrigger : MonoBehaviour
+{
+
+    public Animator anim;
+    public string triggerName;
+
+    public void Set(){
+
+        anim.SetTrigger(triggerName);
+
+    }
+}
