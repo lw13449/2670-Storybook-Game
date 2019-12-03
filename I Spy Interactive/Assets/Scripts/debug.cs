5fde9c4118a994bf42ad32798ff3f30c86d71b80
Lillian Wright <10803934@my.uvu.edu>
2019-12-02 21:02:22 -0700

    assets and building

---
+++ I Spy Interactive/Assets/Scripts/debug.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class debug : ScriptableObject
+{
+    public void OnDebug(string message)
+    {
+        Debug.Log(message);
+    }
+
+    public void onDebug(int number)
+    {
+        Debug.Log(number);
+    }
+    
+}
